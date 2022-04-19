4c068b11958ad4d709f75045aaf1a7d7d307feb9
marek <marek.lenik@lendinvest.com>
2022-04-19 18:04:56 +0100

    initial markup

--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -1,49 +1,76 @@
-<div>
-    <h1>Search products</h1>
+<main>
+  <form>
+    <fieldset>
+      <legend>Search products</legend>
 
-    <form>
-        <label for="property_value">Property value: </label>
+      <label for="property_value">
+        <span>Property value:</span>
         <input wire:model.defer="propertyValue" type="text" id="property_value">
-        <br><br>
-        <label for="deposit_amount">Deposit amount: </label>
+      </label>
+
+      <label for="deposit_amount">
+        <span>Deposit amount:</span>
         <input wire:model.defer="depositAmount" type="text" id="deposit_amount">
-        <br><br>
-        <button type="button" wire:click="$refresh">Find products</button>
-    </form>
+      </label>
 
-    @if ($searchProducts)
-        <h2>Calculation results</h2>
+      <button type="button" wire:click="$refresh">Find products</button>
+    </fieldset>
+  </form>
 
-        <p>LTV: {{ $ltv }}%</p>
-        <p>Loan amount: £{{ number_format($netLoan, 2) }}</p>
+  @if ($searchProducts)
+    <section>
+      <h2>Calculation results</h2>
 
-        <h2>Available products</h2>
+      <p>LTV: {{ $ltv }}%</p>
+      <p>Loan amount: £{{ number_format($netLoan, 2) }}</p>
+    </section>
 
-        <ul>
-            @foreach($products as $product)
-                <li>
-                    <strong>{{ $product->name }}</strong>
-                    <br>Max LTV: {{ $product->max_ltv }}%
-                    <br>Product Fee: {{ $product->fee }}%
-                    <br>Interest rate: {{ $product->interest_rate }}%
-                    <br>Product fee amount: £{{ number_format($product->fee_amount, 2) }}
-                    <br>Gross loan amount: £{{ number_format($product->gross_loan, 2) }}
-                    <br>Monthly interest: £{{ number_format($product->monthly_interest, 2) }}
-                    <br><br>
-                </li>
-            @endforeach
-        </ul>
-    @else
-        <p>Please enter the values above to start searching for products.</p>
-    @endif
+    <section>
+      <h2>Available products</h2>
 
-    @if ($featuredProducts->count())
-        <h2>Featured products</h2>
+      <ul>
+        @foreach($products as $product)
+        <li>
+          <article>
+            <h3>{{ $product->name }}</h3>
+            <dl>
+              <dt>Max LTV</dt>
+              <dd>{{ $product->max_ltv }}%</dd>
 
-        <ul>
-            @foreach($featuredProducts as $product)
-                <li>{{ $product->name }}</li>
-            @endforeach
-        </ul>
-    @endif
-</div>
+              <dt>Product Fee</dt>
+              <dd>{{ $product->fee }}%</dd>
+
+              <dt>Interest rate</dt>
+              <dd>{{ $product->interest_rate }}%</dd>
+
+              <dt>Product fee amount</dt>
+              <dd>£{{ number_format($product->fee_amount, 2) }}</dd>
+
+              <dt>Gross loan amount</dt>
+              <dd>£{{ number_format($product->gross_loan, 2) }}</dd>
+
+              <dt>Monthly interest</dt>
+              <dd>£{{ number_format($product->monthly_interest, 2) }}</dd>
+            </dl>
+          </article>
+        </li>
+        @endforeach
+      </ul>
+    </section>
+
+  @else
+    <p>Please enter the values above to start searching for products.</p>
+  @endif
+
+  @if ($featuredProducts->count())
+    <section>
+      <h2>Featured products</h2>
+
+      <ul>
+        @foreach($featuredProducts as $product)
+          <li>{{ $product->name }}</li>
+        @endforeach
+      </ul>
+    </section>
+  @endif
+</main>
