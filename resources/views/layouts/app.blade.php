04e153e092d3751e0a107757aae3d43ccee02066
marek <marek.lenik@lendinvest.com>
2022-04-20 12:59:46 +0100

    polished markup and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laracon EU Demo</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/app.css">
     @livewireStyles
 </head>
--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -1,76 +1,106 @@
-<main>
-  <form>
-    <fieldset>
-      <legend>Search products</legend>
+<main class="calculator">
+  <div>
+    <div class="box sidebar">
+      <form class="ph-24">
+        <fieldset>
+          <legend>Search products</legend>
 
-      <label for="property_value">
-        <span>Property value:</span>
-        <input wire:model.defer="propertyValue" type="text" id="property_value">
-      </label>
+          <label for="property_value">
+            <span class="label">Property value:</span>
+            <input
+              wire:model.defer="propertyValue"
+              type="text"
+              id="property_value"
+            >
+          </label>
 
-      <label for="deposit_amount">
-        <span>Deposit amount:</span>
-        <input wire:model.defer="depositAmount" type="text" id="deposit_amount">
-      </label>
+          <label for="deposit_amount">
+            <span class="label">Deposit amount:</span>
+            <input
+              wire:model.defer="depositAmount"
+              type="text"
+              id="deposit_amount"
+            >
+          </label>
 
-      <button type="button" wire:click="$refresh">Find products</button>
-    </fieldset>
-  </form>
+          <button
+            wire:click="$refresh"
+            type="button"
+            class="button mt-30"
+          >
+            Find products
+          </button>
+        </fieldset>
+      </form>
+
+      @if ($featuredProducts->count())
+        <aside class="featured ph-24">
+          <h2>Featured products</h2>
+
+          <ul class="featuredList">
+            @foreach($featuredProducts as $product)
+              <li>{{ $product->name }}</li>
+            @endforeach
+          </ul>
+        </aside>
+      @endif
+    </div>
+  </div>
+
 
   @if ($searchProducts)
     <section>
       <h2>Calculation results</h2>
 
-      <p>LTV: {{ $ltv }}%</p>
-      <p>Loan amount: £{{ number_format($netLoan, 2) }}</p>
-    </section>
+      <div class="summary">
+        <div>
+          <strong>LTV:</strong> {{ $ltv }}%
+        </div>
+        <div>
+          <strong>Loan amount:</strong> £{{ number_format($netLoan, 2) }}
+        </div>
+      </div>
 
-    <section>
-      <h2>Available products</h2>
 
-      <ul>
-        @foreach($products as $product)
-        <li>
-          <article>
-            <h3>{{ $product->name }}</h3>
-            <dl>
-              <dt>Max LTV</dt>
-              <dd>{{ $product->max_ltv }}%</dd>
+      <section>
+        <h3>Available products</h3>
 
-              <dt>Product Fee</dt>
-              <dd>{{ $product->fee }}%</dd>
+        <ul class="products">
+          @foreach($products as $product)
+            <li>
+              <article class="box product">
+                <h4>{{ $product->name }}</h4>
 
-              <dt>Interest rate</dt>
-              <dd>{{ $product->interest_rate }}%</dd>
+                <dl class="productDetails">
+                  <dt>Max LTV</dt>
+                  <dd>{{ $product->max_ltv }}%</dd>
 
-              <dt>Product fee amount</dt>
-              <dd>£{{ number_format($product->fee_amount, 2) }}</dd>
+                  <dt>Product Fee</dt>
+                  <dd>{{ $product->fee }}%</dd>
 
-              <dt>Gross loan amount</dt>
-              <dd>£{{ number_format($product->gross_loan, 2) }}</dd>
+                  <dt>Interest rate</dt>
+                  <dd>{{ $product->interest_rate }}%</dd>
 
-              <dt>Monthly interest</dt>
-              <dd>£{{ number_format($product->monthly_interest, 2) }}</dd>
-            </dl>
-          </article>
-        </li>
-        @endforeach
-      </ul>
+                  <dt>Product fee amount</dt>
+                  <dd>£{{ number_format($product->fee_amount, 2) }}</dd>
+
+                  <dt>Gross loan amount</dt>
+                  <dd>£{{ number_format($product->gross_loan, 2) }}</dd>
+
+                  <dt>Monthly interest</dt>
+                  <dd>£{{ number_format($product->monthly_interest, 2) }}</dd>
+                </dl>
+              </article>
+            </li>
+          @endforeach
+        </ul>
+      </section>
     </section>
 
   @else
-    <p>Please enter the values above to start searching for products.</p>
-  @endif
-
-  @if ($featuredProducts->count())
-    <section>
-      <h2>Featured products</h2>
-
-      <ul>
-        @foreach($featuredProducts as $product)
-          <li>{{ $product->name }}</li>
-        @endforeach
-      </ul>
-    </section>
+    <div class="noResults">
+      <strong>We currently do not have any products matching your criteria</strong>
+      <p>Try adjusting your details on the left.</p>
+    </div>
   @endif
 </main>
